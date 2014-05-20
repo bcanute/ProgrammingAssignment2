c409e2e372c48413e494cb324863b1a01a7e4b24
Brian Canute <bcanute@optusnet.com.au>
2014-05-20 14:34:28 +1000

    First working draft.
    Comments to come.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
-## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+    Print("Caching matrix.")
+  }
+  get <- function() {
+    print("Retrieving from cache.")
+    x
+  }
+  setmatrix<- function(m) {
+    m<<-matrix(x)
+  }
+  getmatrix <- function(){
+    m
+  } 
+  list(set = set, 
+       get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
+
+
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(y=matrix(), ...) {
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x
+  m <- solve(y)
+  x$setmatrix(m)
+  m
 }
