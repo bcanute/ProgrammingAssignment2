1c6dd0ef7d6f3c6c365ff960fc0bd94c9aef34ed
Brian Canute <bcanute@optusnet.com.au>
2014-05-24 14:24:05 +1000

    New comments added

--- cachematrix.R
+++ cachematrix.R
@@ -6,17 +6,22 @@
 ##Call x<-makeCacheMatrix
 ##Call new<-cacheSolve(your_matrix)
 ##new will then contain the inverse of your matrix.
+##You must set up a separate cache call variable for each new matrix as the
+##the function does not compare matrices for you.
 
 ## Function - makeCacheMatrix(x = matrix()) 
 ##The argument is actually the inverse of the matrix.
-##When this function is called (by the second function), it checks to see if the 
+##When this function is called (by the second function), having first checked to see if the 
 ##inverse of the matrix is already cached.
 ##If it is there, the function finds it and returns it.
 ##If not, the inverse is cached and returned to the calling program.
 ##This also flags the inverse function as cached and available for future calls. 
 
 makeCacheMatrix <- function(x = matrix()){
-  m <- NULL
+  ## m is going to act as a flag, indicating if Imatrix exists.
+  ##So set m to null, when we first create our list of functions.
+    m <- NULL
+  ##Now create a list of 4 functions, which will be called by the cachesolve function.
   set <- function(y) {
     x <<- y
     m <<- NULL
@@ -32,6 +37,9 @@ makeCacheMatrix <- function(x = matrix()){
   getmatrix <- function(){
     m
   } 
+  ##Returns 4 functions as a list, callable as
+  #x$get()
+  #x$set().....
   list(set = set, 
        get = get,
        setmatrix = setmatrix,
@@ -48,15 +56,18 @@ makeCacheMatrix <- function(x = matrix()){
 ## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(y=matrix(), ...) {
+  ##check if m has anything in it.
   m <- x$getmatrix()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
-  }
+  ## if it is already cached, retrun it to the caller. 
+  ##else calreate the inverse and cache it. 
+  }else
   data <- x
   m <- solve(y)
   x$setmatrix(m)
-  m
+  return(m)
 }
 
 ##Instructions to test the program.
@@ -65,7 +76,7 @@ cacheSolve <- function(y=matrix(), ...) {
 y<-matrix(1:4,2,2)
 y
 
-x<<-makeCacheMatrix()
+x<-makeCacheMatrix()
 x
 
 new<-cacheSolve(y)
